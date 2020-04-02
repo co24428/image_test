74c4484eadbe1fa24d84cce2c3fbc3128f88c4bf
ihwan <ihwan1236@gmail.com>
2020-04-02 08:20:57 +0000

    final_test

--- example.py
+++ example.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-gray_img = cv2.imread('./322868_1100-1100x628.png', cv2.IMREAD_GRAYSCALE)
-unchanged_img = cv2.imread('./322868_1100-1100x628.png', cv2.IMREAD_UNCHANGED)
+gray_img = cv2.imread('./sample_dog.png', cv2.IMREAD_GRAYSCALE)
+unchanged_img = cv2.imread('./sample_dog.png', cv2.IMREAD_UNCHANGED)
 
 cv2.imwrite('gray_test.png', gray_img)
 cv2.imwrite('unchanged_test.png', unchanged_img)
